eaa98136a459a6d6deff552f0b0f6414b29d0a31
UKRoo <ivashko.dmytro@gmail.com>
2019-11-08 22:17:24 +0200

    1-2

---
+++ main.js
@@ -0,0 +1,38 @@
+// 1- Написать функцию, которая принимает 2 числа и возвращает
+//  -1, если первое меньше, чем второе;
+// 1 – если первое больше, чем второе; и 0 – если числа равны.
+
+// function backNumber (a, b) {
+//     if (a < b) {
+//         return "-1";
+//     } else if (a > b) {
+//         return "1";
+//     } else {
+//         console.log(0)
+//     }
+// }
+
+// let a = prompt("Enter A ");
+// let b = prompt("Enter B ");
+
+// backNumber(a ,b);
+
+// 2- Написать функцию, которая вычисляет факториал переданного ей числа.
+
+
+
+
+//   function factoral(x) {
+//     if(x == 0) {
+//         return 1;
+//     }
+//     if(x < 0 ) {
+//         return undefined;
+//     }
+//     for(var i = x; --i; ) {
+//         x *= i;
+//     }
+//      return x;
+// }
+
+  
